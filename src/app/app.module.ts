8bf36c6e35f702a6733d84a1a3c2a10d59cfd82a
priscila.assis <priscila.assis@MTTTKTL1FN2>
2019-03-04 16:12:49 -0300

    Serviços, Promises, Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule  } from '@angular/common/http';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    RodapeComponent,
+    HomeComponent,
+    RestaurantesComponent,
+    DiversaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+
+@Component({
+  selector: 'pri-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService]
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas : Array<Oferta>;
+
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit() {
+    this.ofertasService.getOfertas()
+    .then((ofertas : Oferta[]) => { this.ofertas = ofertas})
+    .catch((param: any) => {console.log(param) });
+  }
+
+}
